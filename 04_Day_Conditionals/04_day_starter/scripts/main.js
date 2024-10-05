095a02fe8b78bc16b83a36a7677ec9248c5b9007
username <ivlevaarina@gmail.com>
2024-10-05 14:07:01 +0600

    day4/task1-easy

--- 04_Day_Conditionals/04_day_starter/scripts/main.js
+++ 04_Day_Conditionals/04_day_starter/scripts/main.js
@@ -1,3 +1,39 @@
-// this is your main.js script
+//Task1
+let age = prompt('Enter your age: ')
 
-alert('Open the browser console whenever you work on JavaScript')
+if (age >= 18) {
+	console.log('You are old enough to drive.')
+} else {
+	console.log('You are left with', 18 - age, 'years to drive.')
+}
+
+//Task2
+let myAge = 16
+let yourAge = prompt('Enter your age: ')
+
+if (myAge > yourAge) {
+	console.log('You are', myAge - yourAge, 'years younger than me.')
+} else if (myAge == yourAge) {
+	console.log('You are the same age as me.')
+} else {
+	console.log('You are ', yourAge - myAge, 'years older than me.')
+}
+
+//Task3
+let a = prompt('Enter number a: ')
+let b = prompt('Enter number b: ')
+
+if (a > b) {
+	console.log('a is greater than b')
+} else {
+	console.log('a is less than b')
+}
+
+a > b ? console.log('a is greater than b') : console.log('a is less than b')
+
+//Task4
+let n = prompt('Enter a number: ')
+
+n % 2 == 0
+	? console.log(n, 'is an even number')
+	: console.log(n, 'is an odd number')
