b9dda6c3a51d12cc7202541c9b5f71361e7064ef
username <ivlevaarina@gmail.com>
2024-10-11 21:30:57 +0600

    day8/task1-easy

--- 08_Day_Objects/08_day_starter/scripts/main.js
+++ 08_Day_Objects/08_day_starter/scripts/main.js
@@ -1,2 +1,31 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+//Task1-easy
+const dog = {
+	//Task3-easy
+	name: 'Bobik',
+	legs: 4,
+	color: 'brown',
+	age: 3,
+	bark: function () {
+		return 'woof woof'
+	},
+	//Task5-easy
+	breed: function () {
+		return Object.assign({}, dog)
+	},
+	getDogInfo: function () {
+		return `I'm ${this.name} and I've ${this.legs} legs. I'm ${this.age} years old and my color is ${this.color}.`
+	},
+}
+
+//Task2-easy
+console.log(dog)
+
+//Task4-easy
+console.log(dog.name)
+console.log(dog.legs)
+console.log(dog.color)
+console.log(dog.age)
+console.log(dog.bark())
+
+console.log(dog.breed())
+console.log(dog.getDogInfo())
