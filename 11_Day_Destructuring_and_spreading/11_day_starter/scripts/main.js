cb26876ad97a1cb1e4c142f7aa1fd2a09a23bfcc
username <ivlevaarina@gmail.com>
2024-10-27 14:03:37 +0600

    day11/task1-easy

--- 11_Day_Destructuring_and_spreading/11_day_starter/scripts/main.js
+++ 11_Day_Destructuring_and_spreading/11_day_starter/scripts/main.js
@@ -1,2 +1,22 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+//Task1-easy
+const constants = [2.72, 3.14, 9.81, 37, 100]
+let [e, pi, gravity, humanBodyTemp, waterBoilingTemp] = constants
+
+console.log(e, pi, gravity, humanBodyTemp, waterBoilingTemp)
+
+//Task2-easy
+const countries2 = ['Finland', 'Estonia', 'Sweden', 'Denmark', 'Norway']
+let [fin, est, sw, den, nor] = countries2
+
+console.log(fin, est, sw, den, nor)
+
+//Task3-easy
+const rectangle = {
+	width: 20,
+	height: 10,
+	area: 200,
+	perimeter: 60,
+}
+
+let { width, height, area, perimeter } = rectangle
+console.log(width, height, area, perimeter)
